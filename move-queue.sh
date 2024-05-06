2a954ca8906b095e0026a4d8a3fd896e8832152d
hussam770 <h.libdeh@gmail.com>
2024-05-06 09:54:33 +0300

    finalize..

--- move-queue.sh
+++ move-queue.sh
@@ -1,1 +1,5 @@
 java -jar k-rabbitmq-cdr.jar --source-type AMQP --source-uri amqp://guest:guest@localhost:5672/vh1 --source-queue source --target-type AMQP --target-uri amqp://guest:guest@localhost:5673/vh2 --target-queue dest
+./pull-rabbitmq-queues-messsages.sh -s hussam-mint-linux --sport 15672:5672 --sauth guest:guest -d hussam-mint-linux --dport 15673:5673 --dauth guest:guest
+./pull-rabbitmq-queues-messsages.sh -r STF -s hussam-mint-linux --sport 15672:5672 --sauth guest:guest -f "../payloads"
+./pull-rabbitmq-queues-messsages.sh -r FTS -f "../payloads" -d hussam-mint-linux --dport 15673:5673 --dauth guest:guest
+./pull-rabbitmq-queues-messsages.sh -r PURGE -d hussam-mint-linux --dport 15673:5673 --dauth guest:guest
--- pull-rabbitmq-queues-messsages.sh
+++ pull-rabbitmq-queues-messsages.sh
@@ -335,10 +335,7 @@ if [ "$direction" = "PURGE" ]; then
 	done < <(jq -rc '.queues[]' ${defintion_file})
 fi
 
-#./pull-rabbitmq-queues-messsages.sh -s hussam-mint-linux --sport 15672:5672 --sauth guest:guest -d hussam-mint-linux --dport 15673:5673 --dauth guest:guest
-#./pull-rabbitmq-queues-messsages.sh -r STF -s hussam-mint-linux --sport 15672:5672 --sauth guest:guest -f "../payloads"
-#./pull-rabbitmq-queues-messsages.sh -r FTS -f "../payloads" -d hussam-mint-linux --dport 15673:5673 --dauth guest:guest
-#./pull-rabbitmq-queues-messsages.sh -r PURGE -d hussam-mint-linux --dport 15673:5673 --dauth guest:guest
+
 
 
 
